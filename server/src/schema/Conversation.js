4a2242d2de1e5f6430e110f67bc1fc833b498776
Nguyen Luong Bang <shirakuyanai@gmail.com>
2025-02-20 11:24:15 +0700

    feat basic routes for front end (#1)
    Co-authored-by: Shiraku Yanai <12289468-shirakuyanai@users.noreply.gitlab.com>

--- server/src/schema/Conversation.js
+++ server/src/schema/Conversation.js
@@ -10,6 +10,12 @@ import User from './User.js'
 
 const Conversation = pgTable('conversation', {
 	conversationId: uuid('conversationId').defaultRandom().primaryKey(),
+	user1Id: uuid('user1Id')
+		.references(() => User.userId)
+		.notNull(),
+	user2Id: uuid('user2Id')
+		.references(() => User.userId)
+		.notNull(),
 })
 
 export default Conversation
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,23 +1,57 @@
-import { useEffect, useState } from 'react'
+import React, { lazy } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Layout from './Pages/layout'
+import LoginPage from './Pages/LoginPage'
+import HomePage from './Pages/HomePage'
+import OverViewPage from './Pages/Overview'
+import ClassPage from './Pages/ClassPage'
+import NotFound from './Components/NotFound'
+import StaffLayout from './Pages/Staff'
+import EditClass from './Pages/Staff/Class/EditClass'
+import AddClass from './Pages/Staff/Class/AddClass'
 
 function App() {
-	const [dataFetched, setDataFetched] = useState(false)
-
-	const fetchData = async () => {
-		const response = await fetch(import.meta.env.VITE_HOST)
-		alert(await response.json())
-		setDataFetched(true)
-	}
-
-	useEffect(() => {
-		if (!dataFetched) {
-			fetchData()
-		}
-	}, [dataFetched])
 	return (
-		<>
-			<p>{import.meta.env.VITE_GREETINGS}</p>
-		</>
+		<Router>
+			<Routes>
+				<Route
+					index
+					path='/login'
+					element={<LoginPage />}
+				/>
+				<Route element={<Layout />}>
+					<Route
+						index
+						path='/'
+						element={<HomePage />}
+					/>
+					<Route
+						path='/overview'
+						element={<OverViewPage />}
+					/>
+
+					<Route
+						index
+						path='/classes'
+						element={<ClassPage />}
+					/>
+					<Route
+						path='*'
+						element={<NotFound />}
+					/>
+				</Route>
+				<Route element={<StaffLayout />}>
+					<Route
+						path='/class/edit'
+						element={<EditClass />}
+					/>
+					<Route
+						path='/class/add'
+						element={<AddClass />}
+					/>
+				</Route>
+			</Routes>
+		</Router>
 	)
 }
 
