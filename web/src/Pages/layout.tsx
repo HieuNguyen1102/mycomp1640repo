361d66ef78a782e9785865dc55bfd394b3319325
Shiraku Yanai <12289468-shirakuyanai@users.noreply.gitlab.com>
2025-02-28 19:06:36 +0700

    feat tailwindcss; feat side bars

--- web/src/Pages/layout.tsx
+++ web/src/Pages/layout.tsx
@@ -1,10 +1,14 @@
 import { Outlet } from 'react-router-dom'
+import Sidebar from '../components/Sidebar'
 
 function Layout() {
 	return (
 		<div className='bg-accent/5 min-h-screen'>
-			<div className='container py-6'>
-				<Outlet />
+			<div className='flex flex-row'>
+				<Sidebar />
+				<div className='flex-1 ml-64 container'>
+					<Outlet />
+				</div>
 			</div>
 		</div>
 	)
